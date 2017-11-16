8c71080cc3ed7ae240657810142b507d55128641
shadow-paw <shadow-paw@users.noreply.github.com>
2017-11-16 23:05:15 +0800

    #example http example

---
+++ example/http/glue/mac/AppDelegate.mm
@@ -0,0 +1,47 @@
+#import "AppDelegate.h"
+#import "CatView.h"
+#include "bootapp.h"
+
+// ----------------------------------------------------------------------------
+@interface AppDelegate()
+@property (nonatomic) cat::Kernel* m_kernel;
+@end
+
+@implementation AppDelegate
+// ----------------------------------------------------------------------------
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+    return YES;
+}
+// ----------------------------------------------------------------------------
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    NSWindow *window = [[[NSApplication sharedApplication] windows] objectAtIndex:0];
+    CatView* view = (CatView*)window.contentViewController.view;
+
+    cat::PlatformSpecificData psd;
+    psd.rootview = (__bridge void*)view;
+    self.m_kernel = new cat::Kernel();
+    if (!self.m_kernel->init(psd)) {
+        exit(0);
+        return;
+    }
+    [view setKernel:self.m_kernel];
+    self.m_kernel->vfs()->mount("/assets/", new cat::FileDriver([[[NSBundle mainBundle] resourcePath] UTF8String]));
+    self.m_kernel->context_restored();
+    self.m_kernel->startup();
+    self.m_kernel->run(new app::BootApp());
+    [view startAnimation];
+}
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    if (self.m_kernel) {
+        NSWindow *window = [[[NSApplication sharedApplication] windows] objectAtIndex:0];
+        CatView* view = (CatView*)window.contentViewController.view;
+        [view stopAnimation];
+        [view setKernel:nullptr];
+        self.m_kernel->shutdown();
+        self.m_kernel->fini();
+        delete self.m_kernel;
+        self.m_kernel = nullptr;
+    }
+}
+// ----------------------------------------------------------------------------
+@end
