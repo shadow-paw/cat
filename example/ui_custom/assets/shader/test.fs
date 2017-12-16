473da56c612eab4f2b6bb8db5382af489e5d6879
shadow-paw <shadow-paw@users.noreply.github.com>
2017-12-16 13:54:55 +0800

    updated example shader

--- example/ui_custom/assets/shader/test.fs
+++ example/ui_custom/assets/shader/test.fs
@@ -1,18 +1,24 @@
   #ifdef GL_ES
     precision highp float;
   #endif
+    uniform vec4 uClipping;
     uniform sampler2D uTex0;
     uniform float uTime;
   #if __VERSION__ >= 140
-    in  vec4 vColor;
-    in  vec2 vTexcoord;
-    out vec4 oFragColor;
+    in  lowp vec4 vColor;
+    in       vec2 vTexcoord;
+    out      vec4 oFragColor;
   #else
-    varying vec4 vColor;
-    varying vec2 vTexcoord;
+    varying lowp vec4 vColor;
+    varying      vec2 vTexcoord;
   #endif
   const float PI = 3.1415926535;
+  float inside(vec2 v, vec2 bottomleft, vec2 topright) {
+      vec2 s = step(bottomleft, v) - step(topright, v);
+      return s.x * s.y;   
+  }
   void main() {
+      float alpha = inside(gl_FragCoord.xy, uClipping.xy, uClipping.zw);
       float aperture = 178.0;
       float apertureHalf = 0.5 * aperture * (PI / 180.0);
       float maxFactor = sin(apertureHalf);
@@ -30,8 +36,8 @@
           uv = vTexcoord.xy;
       }
     #if __VERSION__ >= 140
-      oFragColor = vColor * texture2D(uTex0, uv);
+      oFragColor = vColor * texture2D(uTex0, uv) * vec4(1, 1, 1, alpha);
     #else
-      gl_FragColor = vColor * texture2D(uTex0, uv);
+      gl_FragColor = vColor * texture2D(uTex0, uv) * vec4(1, 1, 1, alpha);
     #endif
   }
--- example/ui_custom/assets/shader/test.vs
+++ example/ui_custom/assets/shader/test.vs
@@ -1,22 +1,22 @@
   #ifdef GL_ES
     precision highp float;
   #endif
-    uniform vec2 uScreenHalf;
+    uniform vec2 uCenterMultiplier;
   #if __VERSION__ >= 140
-    in  vec4 inPosition;
-    in  vec4 inColor;
-    in  vec2 inTexcoord;
-    out vec4 vColor;
-    out vec2 vTexcoord;
+    in       vec4 inPosition;
+    in  lowp vec4 inColor;
+    in       vec2 inTexcoord;
+    out lowp vec4 vColor;
+    out      vec2 vTexcoord;
   #else
-    attribute vec2 inPosition;
-    attribute vec4 inColor;
-    attribute vec2 inTexcoord;
-    varying   vec4 vColor;
-    varying   vec2 vTexcoord;
+    attribute      vec2 inPosition;
+    attribute lowp vec4 inColor;
+    attribute      vec2 inTexcoord;
+    varying   lowp vec4 vColor;
+    varying        vec2 vTexcoord;
   #endif
   void main() {
       vColor = inColor;
       vTexcoord = inTexcoord;
-      gl_Position = vec4(inPosition.x / uScreenHalf.x - 1.0, inPosition.y / uScreenHalf.y - 1.0, 0.0, 1.0);
+      gl_Position = vec4(inPosition.xy * uCenterMultiplier.xy - 1.0, 0.0, 1.0);
   }
