8005499f9981090b9438abdeef8a04f720a3887e
shadow-paw <shadow-paw@users.noreply.github.com>
2018-01-28 13:19:05 +0800

    common bootstrap for mac/ios

--- example/ui_basic/glue/ios/CatView.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#include "libcat.h"
-
-@interface CatView : UIView
-- (void) setKernel:(cat::Kernel*)kernel;
-- (void) startAnimation;
-- (void) stopAnimation;
-- (void) drawView:(id)sender;
-@end
